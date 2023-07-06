3cbeb96998b14bf159b8084819c90068f0c60eae
Jame Chaney <macneilslt@gmail.com>
2023-07-06 19:53:54 -0400

    Express with middleware

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
 app.use((req, res) => {
-	console.log(req.headers);
+	//console.log(req.headers); now logged by morgan
 	res.statusCode = 200;
 	res.setHeader = ('Content-Type', 'text/html');
 	res.end('<html><body><h1>This is an Express Sever</h1></body></html>');
