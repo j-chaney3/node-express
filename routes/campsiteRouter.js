5306e3dc06d9c751fd6fe161f5147f1dccaf3799
Jame Chaney <macneilslt@gmail.com>
2023-07-08 11:28:44 -0400

    Campsite Routes

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -3,31 +3,48 @@ const campsiteRouter = express.Router();
 
 campsiteRouter
 	.route('/')
-
 	.all((req, res, next) => {
 		res.statusCode = 200;
 		res.setHeader('Content-Type', 'text/plain');
-		next(); //passes control of application routing to the next relevant (get to get,post to post...) routing method after this one
+		next();
 	})
-
-	//campsite endpoints
 	.get((req, res) => {
-		res.end('Will send all campites to you');
+		res.end('Will send all the campsites to you');
 	})
-
 	.post((req, res) => {
 		res.end(
-			`Will add the campsites: ${req.body.name} with description: ${req.body.description}`
+			`Will add the campsite: ${req.body.name} with description: ${req.body.description}`
 		);
 	})
-
 	.put((req, res) => {
 		res.statusCode = 403;
-		res.end('PUT operation not supported on /campsites');
+		res.end('PUT operation not su:campsiteIdpported on /campsites');
 	})
-
 	.delete((req, res) => {
 		res.end('Deleting all campsites');
+	});
+
+campsiteRouter
+	.route('/:campsiteId')
+	.all((req, res, next) => {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		next();
+	})
+	.get((req, res) => {
+		res.end(`Will send you campsite with id of ${req.params.campsiteId}`);
+	})
+	.post((req, res) => {
+		res.statusCode = 403;
+		res.end('Not supported');
+	})
+	.put((req, res) => {
+		res.end(
+			`Will update the campsite: ${req.body.name} and description: ${req.body.description}`
+		);
+	})
+	.delete((req, res) => {
+		res.end(`Deleting campsite with id: ${req.params.campsiteId}`);
 	});
 
 module.exports = campsiteRouter;
